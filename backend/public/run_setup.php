ad552c28312ebf471e3945837185d7c68b96ae5b
Ajit Kumar KV <ajitsands@gmail.com>
2026-08-14 03:37:33 +0530

    Disable caching on run_setup.php debugger script

--- backend/public/run_setup.php
+++ backend/public/run_setup.php
@@ -3,6 +3,11 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+// Prevent browser caching
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Pragma: no-cache');
+header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
+
 require_once __DIR__ . '/../core/Model.php';
 
 try {
